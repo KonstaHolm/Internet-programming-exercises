df96a61b23f665070bc7b83a8549256f7d969566
KonstaHolm <t8hoko00@students.oamk.fi>
2019-09-10 19:29:12 +0300

    exercise2 valmis

--- menu.php
+++ menu.php
@@ -14,7 +14,7 @@
       		<li><a href="index.php">Main page</a></li>
       		<li><a href="basic.php">PHP Basic</a></li>
       		<li><a href="arrays.php">PHP Arrays</a></li>
-      		<li><a href="./post.php">PHP POST</a></li>
+      		<li><a href="post.php">PHP POST</a></li>
       		<li><a href="get.php">PHP GET</a></li>
       		<li><a href="database.php">Database</a></li>
       		<li><a href="session.php">Session</a></li>
@@ -22,9 +22,3 @@
       </div>
       <div id="content">
         This is the content
-      </div>
-      <div id="footer">
-        This is the footer
-      </div>
-  </body>
-</html>

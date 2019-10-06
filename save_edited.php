757d5b76bf48edfc6511f6694f43c961dd6c59eb
KonstaHolm <t8hoko00@students.oamk.fi>
2019-10-06 23:15:24 +0300

    ex6 valmis

--- save_edited.php
+++ save_edited.php
@@ -1,13 +1,20 @@
-<?php include "menu.php"; ?>
-<?php include "connection.php"; ?>
+
 <?php
-print_r($_POST);
+include "connection.php";
 $stmt=$db->prepare("UPDATE customers SET fname=:en, LNAME=:sn WHERE id_customers=:id");
 $stmt->bindParam(':id',$_POST['id']);
 $stmt->bindParam(':en',$_POST['en']);
 $stmt->bindParam(':sn',$_POST['sn']);
-$stmt->execute();
+if($stmt->execute())
+{
+  header('Location:database.php');
+}
+else
+{
+  include "menu.php";
+  echo 'Päivitys epäonnistui';
+  include "footer.php";
+}
 
 echo 'Päivitys onnistui';
 ?>
-<?php include "footer.php"; ?>

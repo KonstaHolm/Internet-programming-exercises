fddb91d0e649dbaafdb43b84f99c3f9227fdb09f
KonstaHolm <t8hoko00@students.oamk.fi>
2019-10-06 20:49:03 +0300

    ex6 valmis

---
+++ add_customer.php
@@ -0,0 +1,20 @@
+<?php include "menu.php"; ?>
+<?php include "connection.php"; ?>
+<?php
+print_r($_POST);
+echo "<br>";
+//seuraava koodi sisältää SQL-injectionin
+/*$add='INSERT INTO intohj VALUES("'.$_POST['en'].'","'.$_POST['sn'].'")';
+echo $add;*/
+$stmt=$db->prepare("INSERT INTO customers VALUES(:id, :en, :sn)");
+$stmt->bindParam(':id', $_POST['id']);
+$stmt->bindParam(':en', $_POST['en']);
+$stmt->bindParam(':sn', $_POST['sn']);
+
+$stmt->execute();
+echo 'Lisättiin asiakas: '.$_POST['id'].' '.$_POST['en'].' '.$_POST['sn'];
+?>
+
+
+
+<?php include "footer.php"; ?>
